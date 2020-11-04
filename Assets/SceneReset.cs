b54e49a5dbdefa2d47a92f2a2005d1d1d254d2f7
eM-x-Pee <petrovicmatej4@gmail.com>
2020-11-04 11:09:13 +0100

    .

---
+++ Assets/SceneReset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneReset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionReset(Collision other)
+    {
+        if (other.gameObject.tag == "Finger")
+            {
+                SceneManager.LoadScene("Main");
+            }
+        
+    }
+}
